4626b316f2dbf359e45a53e4b4b1ddcfa490bc15
Khaled <khaledahmed165041@gmail.com>
2026-08-03 13:26:52 +1200

    Python lab activity

---
+++ week 2/task1_profile.py
@@ -0,0 +1,14 @@
+name=input("ENTER FULL NAME: ")
+age=(input("enter your age"))
+ageconvertion=int(age) + 1
+height=input("enter your height in meters:")
+
+favouriteprogramminglanguage=input("enter your favourite programming language")
+print("--------------Profile Summary---------")
+print("Name: ", name)
+print("Next year you will be",ageconvertion,"years old",".")
+print("Your height in cm is",height,"cm",".")
+print("You love programming in",favouriteprogramminglanguage,"!")
+
+print("---------------------------------")
+
---
+++ week 2/task2_grade.py
@@ -0,0 +1,18 @@
+assingmentmark=float (input("out of 100"))
+testmark=float(input("out of 100"))
+exammark=float(input("out of 100"))
+assingment=(assingmentmark/100)*30
+test=(testmark/100)*30
+exam=(exammark/100)*40
+finalgrade=(assingment+test+exam)
+print("-------STUDENT RESULT------")
+print ("Assingment Mark",assingmentmark)
+print("Test Mark",testmark)
+print("Exam Mark",exammark)
+
+print("Assingment Contribution",(assingment))
+print("Test Contibution",(test))
+print ("Exam Contribution",(exam))
+
+print("Final Grade",(finalgrade))
+
